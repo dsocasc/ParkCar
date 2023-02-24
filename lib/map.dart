ae1c9049a258af4863acbe205ef857ea92d67e45
Diego Horacio Socas Castro <d.socas@udc.es>
2023-02-24 18:01:35 +0100

    arreglo mapa

--- lib/map.dart
+++ lib/map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatelessWidget{
 
@@ -43,8 +44,8 @@ class NewMap extends State<Map>{
   Widget build(BuildContext context){
     return FlutterMap(
       options: MapOptions(
-        //center: LatLng(51.509364, -0.128928),
-        zoom: 9.2,
+        center: LatLng(43.3713500, -8.3960000),
+        zoom: 14.5,
       ),
       nonRotatedChildren: [
         AttributionWidget.defaultWidget(
